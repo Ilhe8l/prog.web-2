c1c3ad821d0cbcdcd6b533bd88a9ba467606563d
guilherme8l <101971954+Ilhe8l@users.noreply.github.com>
2022-08-25 07:58:36 -0300

    Update java1.js

--- java1.js
+++ java1.js
@@ -1,47 +1,49 @@
-document.addEventListener('DOMContentLoaded', teste)
-//primeira função
-function teste() {
-    const ol = document.createElement('ol')
-    ol.id = 'sumario'
-    document.body.prepend(ol)
-    const secoes =document.querySelectorAll('h2')
-    	for(let i=0; i<secoes.length; i++) {
-    	    const li = criarItem(`#sec${i+1}`, secoes[i].textContent)
-    	    ol.appendChild(li)
-    	    alterarH2(secoes[i], i+1)
+document.addEventListener("DOMContentLoaded", teste) 
+
+//função 1
+function teste(){
+	const ol = document.createElement('ol')
+	ol.id = 'sumário'
+	document.body.prepend(ol)
+	const secoes = document.querySelectorAll('h2')
+	for(let i=0; i<secoes.length; i++) {
+		const li = criarItem(`#sec${i+1}`, secoes[i].textContent)
+		ol.appendChild(li) 
+		alterarH2(secoes[i], i+1) 
+	}
 }
 
-//segunda função
-function alterarH2(elem, secnum) {
-    inserirNumero(elem, secnum)
-    elem.id = `sec${secnum}`
-    const voltar = criarLink('#', 'voltar')
-    inserirDepois(elem, voltar)
+//função 2
+function alterarH2(elem, secnum){ 
+	inserirNumero(elem, secnum)
+	elem.id = `sec${secnum}`
+	const voltar = criarLink('#', 'voltar') 
+	inserirDepois(elem, voltar)
 }
 
-//terceira função 
-function criarItem(href, texto) {
-    const li = document.createElement('li')
-    const linque = criarLink(href,texto)
-    li.appendChild(linque)
-    return li
+//função 3
+function criarItem(href, texto){
+	const li = document.createElement('li')
+	const linque = criarLink(href,texto)
+	li.appendChild(linque)
+	return li
 }
 
-//quarta função 
+//função 4
 function inserirNumero(elem, numero) {
-    elem.textContent = `${numero}. ${elem.textContent}`
+	elem.textContent = `${numero}. ${elem.textContent}`
 }
 
-//quinta função
+//função 5
 function criarLink(href, texto) {
-    const linque = document.createElement('a')
-    linque.setAttribute('href', href)
-    linque.textContent = texto
-    return linque   
+	const linque = document.createElement('a') 
+	linque.setAttribute('href', href) 
+	linque.textContent = texto
+	return linque 
 }
 	
-//sexta função 
+//função 6
 function inserirDepois(de, elem) {
-    const proximoIrmao = de.nextElementSibling;
-    de.parentElement.insertBefore(elem,proximoIrmao)
+	const proximoIrmao = de.nextElementSibling;
+	de.parentElement.insertBefore(elem,proximoIrmao)
 }
