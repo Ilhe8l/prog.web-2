f804c47c23f028a674a05f254d4da04218afa829
guilherme8l <101971954+Ilhe8l@users.noreply.github.com>
2022-09-08 08:08:06 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,30 +1,30 @@
-const tamanhocasas = 40;
-let peçaId = 0;
-document.body.append(criartabuleiro());
+const tamanhocasa = 40;
+let pecaId = 0;
+document.body.append(criartabu());
 
-function criartabuleiro() {
+function criartabu() {
     const tamanho = 8;
     let tabela = document.createElement('table');
 
     tabela.style.borderStyle = 'solid';
     tabela.style.borderSpacing = 0;
     tabela.style.margin = 'auto';
-    
+
     for (let i = 0; i < tamanho; i++) {
         let linha = document.createElement('tr');
         tabela.append(linha);
         for (let j = 0; j < tamanho; j++) {
             let casa = document.createElement('td');
             linha.append(casa);
-        
-            casa.style.width = `${tamanhocasas}px`;
-            casa.style.height = `${tamanhocasas}px`;
+
+            casa.style.width = `${tamanhocasa}px`;
+            casa.style.height = `${tamanhocasa}px`;
             if (i % 2 == j % 2) {
                 casa.style.backgroundColor = 'black';
                 if (i * 8 + j <= 24) {
-                    casa.append(criarpeça('black'));
+                    casa.append(criarpeca('black'));
                 } else if (i * 8 + j >= 40) {
-                    casa.append(criarpeça('red'));
+                    casa.append(criarpeca('red'));
                 }
             } else {
                 casa.style.backgroundColor = 'white';
@@ -32,11 +32,12 @@ function criartabuleiro() {
         }
     };
     return tabela;
-}  
-function criarpeça(cor) {
+}
+
+function criarpeca(cor) {
     let imagem = document.createElement('img');
     imagem.setAttribute('src', `img/${cor}.png`);
-    imagem.setAttribute('width', `${tamanhocasas-4}px`);
-    imagem.setAttribute('height', `${tamanhocasas-4}px`);
+    imagem.setAttribute('width', `${tamanhocasa-4}px`);
+    imagem.setAttribute('height', `${tamanhocasa-4}px`);
     return imagem;
 }
