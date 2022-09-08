212ca756854bdc6011f6e663f749f2a7cdef5c41
guilherme8l <101971954+Ilhe8l@users.noreply.github.com>
2022-09-07 23:40:46 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,42 +1,43 @@
-const tamanhocasas = 40;
-let peçaId = 0;
-document.body.append(criartabuleiro());
+const tamanhoCelula = 40;
+let pecaId = 0;
+document.body.append(criaTabuleiro());
 
-function criartabuleiro() {
+function criaTabuleiro() {
     const tamanho = 8;
     let tabela = document.createElement('table');
 
     tabela.style.borderStyle = 'solid';
     tabela.style.borderSpacing = 0;
     tabela.style.margin = 'auto';
-    
+
     for (let i = 0; i < tamanho; i++) {
         let linha = document.createElement('tr');
         tabela.append(linha);
         for (let j = 0; j < tamanho; j++) {
-            let casa = document.createElement('td');
-            linha.append(casa);
-        
-            casa.style.width = `${tamanhocasas}px`;
-            casa.style.height = `${tamanhocasas}px`;
+            let celula = document.createElement('td');
+            linha.append(celula);
+
+            celula.style.width = `${tamanhoCelula}px`;
+            celula.style.height = `${tamanhoCelula}px`;
             if (i % 2 == j % 2) {
-                casa.style.backgroundColor = 'black';
+                celula.style.backgroundColor = 'black';
                 if (i * 8 + j <= 24) {
-                    casa.append(criarpeça('black'));
+                    celula.append(criaPeca('black'));
                 } else if (i * 8 + j >= 40) {
-                    casa.append(criarpeça('red'));
+                    celula.append(criaPeca('red'));
                 }
             } else {
-                casa.style.backgroundColor = 'white';
+                celula.style.backgroundColor = 'white';
             }
         }
     };
     return tabela;
-    
-function criarpeça(cor) {
+}
+
+function criaPeca(cor) {
     let imagem = document.createElement('img');
     imagem.setAttribute('src', `img/${cor}.png`);
-    imagem.setAttribute('width', `${tamanhocasas-4}px`);
-    imagem.setAttribute('height', `${tamanhocasas-4}px`);
+    imagem.setAttribute('width', `${tamanhoCelula-4}px`);
+    imagem.setAttribute('height', `${tamanhoCelula-4}px`);
     return imagem;
-}               
+}
